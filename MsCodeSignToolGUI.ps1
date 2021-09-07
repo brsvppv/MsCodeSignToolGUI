28470f82143d6ab99624a6aec41c38d82e00b76f
Borislav G. Popov <65204241+brsvppv@users.noreply.github.com>
2021-09-07 16:57:56 +0300

    addingFunction

--- MsCodeSignToolGUI.ps1
+++ MsCodeSignToolGUI.ps1
@@ -13,8 +13,8 @@ xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         Title="CodeSignGUI" Height="200" Width="355" ResizeMode="NoResize" WindowStartupLocation="CenterScreen">
         <Grid Background="{DynamicResource {x:Static SystemColors.WindowBrushKey}}" Height="200" Width="355" VerticalAlignment="Center" HorizontalAlignment="Center" >
             <TextBox Name="txtSignToolPath" Text="Select Code SignTool Path" HorizontalAlignment="Left" Height="20" Width="300" Margin="15,31,0,0" VerticalAlignment="Top"  FontSize="11" IsReadOnly="true"/>
-            <TextBox Name="txtCertificatePath" Text="Select Code Signing Certificate" HorizontalAlignment="Left" Height="20" Width="300" Margin="15,56,0,0" VerticalAlignment="Top"  FontSize="11" IsReadOnly="true"/>
-            <TextBox Name="txtFileToSignPath" Text="Select File to Sign" HorizontalAlignment="Left" Height="20" Width="300" Margin="15,83,0,0" VerticalAlignment="Top" FontSize="11" IsReadOnly="true"/>   
+            <TextBox Name="txtCertificatePath" Text="Select Code Signing Certificate" HorizontalAlignment="Left" Height="20" Width="300" Margin="15,56,0,0" VerticalAlignment="Top"  FontSize="11" IsReadOnly="false"/>
+            <TextBox Name="txtFileToSignPath" Text="Select File to Sign" HorizontalAlignment="Left" Height="20" Width="300" Margin="15,83,0,0" VerticalAlignment="Top" FontSize="11" IsReadOnly="false"/>   
             <Button Name="btnSelecSignToolPath" Content="..." Margin="314,31,0,0" VerticalAlignment="Top" Height="20"  Width="27" HorizontalAlignment="Left"/>
             <Button Name="btnSelectCert" Content="..." HorizontalAlignment="Left" Margin="314,56,0,0" VerticalAlignment="Top" Width="27" Height="20"/>
             <Button Name="btnFile" Content="..." HorizontalAlignment="Left" Margin="314,83,0,0" VerticalAlignment="Top" Width="27" Height="20"/>
@@ -33,7 +33,7 @@ catch { Write-Host "Unable to load Windows.Markup.XamlReader"; exit }
 $xaml.SelectNodes("//*[@Name]") | ForEach-Object { Set-Variable -Name ($_.Name) -Value $Form.FindName($_.Name) }
 
 $fdAlg = "SHA256"
-$tURL = "http://timestamp.digicert.com "
+$tURL = "http://timestamp.digicert.com"
 Function SelectCertificate { 
     $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
         Multiselect = $false # Multiple files can be chosen
@@ -102,55 +102,64 @@ function SelectDirectory {
         }
     }
 }
+function ButtonEnable{
+    if ($txtCertificatePath.Text -eq "Select Code Signing Certificate" -bor $txtFileToSignPath.Text -eq "Select File to Sign" -bor $pswdbCert.Password -eq ""){
+        $btnPerformCodeSign.IsEnabled = $false
+    }
+    else{
+        $btnPerformCodeSign.IsEnabled = $true
+    }
+}
 $Form.Add_Loaded( {
+        $btnPerformCodeSign.IsEnabled = $false
         $txtSignToolPath.Text = "C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64"
         #$pswdbCert.PasswordChar = '*'
     })
+$txtCertificatePath.Add_TextChanged({
+    ButtonEnable
+})
+$txtFileToSignPath.Add_TextChanged({
+    ButtonEnable
+})
+$pswdbCert.Add_LostFocus({
+    ButtonEnable
+})
 
 #default sign tool location:
 #"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64"
-$signToolLocaion = $txtSignToolPath
-#file to sign 
-#"D:\@CLOUD\GitHub_brsvppv\NavBulkUsers\CreateBulkBcNavUsers.exe"
-
-#Code signing certiciate:
-#"C:\!DO_NOT_DELETE\CodeSignCert\Team VISION-Bulgaria Ltd.pfx"
-$PfxCertificate = $txtCertificatePath
-
-#$PfxPassword = (Read-host Password:)
 $btnSelecSignToolPath.Add_Click( {
         SelectDirectory
-        
     })
 
 $btnSelectCert.Add_Click( {
         SelectCertificate
         
-        #$password = Read-Host 'What is your password?' -AsSecureString
     })
 $btnFile.Add_Click( {
         SelectFileToSign
     })
+
 #Set-Location "$signToolLocaion"
 $btnPerformCodeSign.Add_Click( {
-    
-    #Write-Host $txtSignToolPath.Text
-    #Write-host $txtCertificatePath.Text
-    #Write-host $txtFileToSignPath.Text
-    #$Script:password = $pswdbCert.Password
-    #write-host $pswdbCert.Password
-   # write-host "$password"
     Set-Location $txtSignToolPath.Text
-    Try{
-        ./signtool.exe sign /t $tURL /fd $fdAlg /f $txtCertificatePath.Text /p $password $txtFileToSignPath.Text    
-        [System.Windows.MessageBox]::Show("File has been signed", 'Signed Succesfully', 'OK', 'information')
-
+    Write-Host $txtSignToolPath.Text
+    Write-host $txtCertificatePath.Text
+    Write-host $txtFileToSignPath.Text
+    $Script:password = $pswdbCert.Password
+    write-host $pswdbCert.Password
+    #./signtool.exe sign /t "$tURL" /fd $fdAlg /f $txtCertificatePath.Text /p $pswdbCert.Password $txtFileToSignPath.Text
+    $SignTool = "./signtool.exe sign " + "/t $tURL /fd $fdAlg /f $txtCertificatePath.Text /p $pswdbCert.Password $txtFileToSignPath.Text"
+   # write-host "$password"
+    try{
+        
+        Invoke-Expression -Command: "$SignTool" -ErrorAction STOP
+    
+        #./signtool.exe sign /t "$tURL" /fd $fdAlg /f $txtCertificatePath.Text /p $pswdbCert.Password $txtFileToSignPath.Text
     }
     Catch{
-        [System.Windows.MessageBox]::Show("An error occured during signing: $_", 'Error', 'OK', 'Error')
-
+        [System.Windows.MessageBox]::Show("An Error Occured During Signing  $_", 'ERROR', 'OK', 'Error') 
     }
+    [System.Windows.MessageBox]::Show("Completed", 'File Sign Result', 'OK', 'Information')
+})
 
-    })
-
-$Form.ShowDialog() | out-null
+$Form.ShowDialog() | out-null   
